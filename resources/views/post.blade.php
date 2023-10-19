d7fbf80ad4aadd8c1f3529914fdf90b07e4439ab
victor braz <victor.90braz@gmail.com>
2023-10-19 10:22:06 +0200

    @yield('content')

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
-    <title>Blog</title>
-</head>
-<body>
+
+@extends('layout')
+
+@section('content')
     <div class="container">
         <article>
             <h1><?= $post->title ?></h1>
@@ -16,5 +9,6 @@
         </article>
         <a href="/">Go Back</a>
     </div>
-</body>
-</html>
+@endsection
+
+
