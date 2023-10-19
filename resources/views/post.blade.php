3f23962f7187e8747cce6e4912059fbfd922bda3
victor braz <victor.90braz@gmail.com>
2023-10-19 10:41:09 +0200

    <x-button class="my-button">Send Post</x-button>

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,10 @@
-
-@extends('layout')
-
-@section('content')
+<x-layout>
     <div class="container">
         <article>
-            <h1><?= $post->title ?></h1>
-            <div><?= $post->body ?></div>
+            <h1>{{ $post->title }}</h1>
+            <div>{!! $post->body !!}</div>
+            <x-button class="my-button">Send Post</x-button>
         </article>
         <a href="/">Go Back</a>
     </div>
-@endsection
-
-
+</x-layout>
